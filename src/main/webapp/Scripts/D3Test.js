eaeb06bbeba9e22d4e8404ac3fd05cba48c5dea3
mirzabaig <mirzabaig@labpc521.computing.dundee.ac.uk>
2016-10-03 13:14:05 +0100

    Dataset

--- src/main/webapp/Scripts/D3Test.js
+++ src/main/webapp/Scripts/D3Test.js
@@ -3,6 +3,7 @@ function D3Draw()
     var dataset = [5,6,2,8,9];
     d3.select("body")
       .selectall("p")
+      .data(dataset)
       .enter()
       .append("p")        
       .text("Multiple text lines");
