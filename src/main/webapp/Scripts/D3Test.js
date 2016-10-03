c336742581845de0f8aa7c9b4669bb790a8068af
mirzabaig <mirzabaig@labpc521.computing.dundee.ac.uk>
2016-10-03 13:00:01 +0100

    JS files

--- src/main/webapp/Scripts/D3Test.js
+++ src/main/webapp/Scripts/D3Test.js
@@ -1,6 +1,8 @@
 function D3Draw()
 {
-    d3.select("body").append("p").text("D3 text from JS file");
+    d3.select("body")
+      .append("p")
+      .text("D3 text from JS file");
 }
 
-window.onload=d3Draw;
+window.onload=D3Draw;
