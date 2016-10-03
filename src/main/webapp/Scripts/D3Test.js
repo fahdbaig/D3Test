358f0eab628e5a05033c28315b66e510e4473521
mirzabaig <mirzabaig@labpc521.computing.dundee.ac.uk>
2016-10-03 13:16:56 +0100

    Dataset

--- src/main/webapp/Scripts/D3Test.js
+++ src/main/webapp/Scripts/D3Test.js
@@ -1,8 +1,7 @@
 function D3Draw()
 {
     var dataset = [5,6,2,8,9];
-    d3.select("body")
-      .selectall("p")
+    d3.select("body").selectAll("p")      
       .data(dataset)
       .enter()
       .append("p")        
