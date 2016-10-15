9ca1793c643d8ee4a773315f39e34d9d5e2c8901
mirzabaig <mirzabaig@labpc521.computing.dundee.ac.uk>
2016-10-15 17:08:33 +0100

    Geo Mapping

--- src/main/webapp/Scripts/D3Test.js
+++ src/main/webapp/Scripts/D3Test.js
@@ -88,7 +88,7 @@ function MapDraw(townsData)
                                .center(d3.geoCentroid(uk))
                                .translate([width/2,height/2]);
             */
-           var projection = d3.geoAlbers()
+           var projection = d3.geo.Albers()
                               .center([0,55,4])
                               .rotate([4.4,0])
                               .parallels([50,60])
